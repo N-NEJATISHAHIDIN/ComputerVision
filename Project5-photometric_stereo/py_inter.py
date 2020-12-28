3dd4b50d77064dea58f80707631609e16c136c49
Negar Nejati <negar@Negars-MacBook-Pro-2.local>
2020-12-28 14:44:53 -0500

    CS 682 all Projects

---
+++ Project5-photometric_stereo/py_inter.py
@@ -0,0 +1,17 @@
+## Plotting Code 3D: Open in interactive terminal so that you can "look around"
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+X = np.load("points.npy")
+colors = np.load("colors.npy")
+print(colors.shape)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[0, :], X[1, :], X[2, :], c=colors)
+ax.set_xlim3d([-750, 750])
+ax.set_ylim3d([-750, 750])
+ax.set_zlim3d([-750, 750])
+plt.show()
